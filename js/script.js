c9121befd112fa0c045301aa611e1990430e31e4
Teagan Wrest <wrestteagan@gmail.com>
2015-08-27 03:15:02 -0700

    Different

---
+++ js/script.js
@@ -0,0 +1,145 @@
+$(function(){
+		$('#slideUpBtn').click(function() {
+			$(this).each(function(){
+					$('#object').removeClass();
+					$('#object').addClass("slideUp");
+					$('#class-info span').html('.slideUp');
+				});
+		});
+
+		$('#slideDownBtn').click(function() {
+			$(this).each(function(){
+					$('#object').removeClass();
+					$('#object').addClass("slideDown");
+					$('#class-info span').html('.slideDown');
+				});
+		});
+
+		$('#slideLeftBtn').click(function() {
+			$(this).each(function(){
+					$('#object').removeClass();
+					$('#object').addClass("slideLeft");
+					$('#class-info span').html('.slideLeft');
+				});
+		});
+
+		$('#slideRightBtn').click(function() {
+			$(this).each(function(){
+					$('#object').removeClass();
+					$('#object').addClass("slideRight");
+					$('#class-info span').html('.slideRight');
+				});
+		});
+
+		$('#slideExpandUpBtn').click(function() {
+			$(this).each(function(){
+					$('#object').removeClass();
+					$('#object').addClass("slideExpandUp");
+					$('#class-info span').html('.slideExpandUp');
+				});
+		});
+
+		$('#expandUpBtn').click(function() {
+			$(this).each(function(){
+					$('#object').removeClass();
+					$('#object').addClass("expandUp");
+					$('#class-info span').html('.expandUp');
+				});
+		});
+
+		$('#fadeInBtn').click(function() {
+			$(this).each(function(){
+					$('#object').removeClass();
+					$('#object').addClass("fadeIn");
+					$('#class-info span').html('.fadeIn');
+				});
+		});
+
+		$('#expandOpenBtn').click(function() {
+			$(this).each(function(){
+					$('#object').removeClass();
+					$('#object').addClass("expandOpen");
+					$('#class-info span').html('.expandOpen');
+				});
+		});
+
+		$('#bigEntranceBtn').click(function() {
+			$(this).each(function(){
+					$('#object').removeClass();
+					$('#object').addClass("bigEntrance");
+					$('#class-info span').html('.bigEntrance');
+				});
+		});
+
+		$('#hatchBtn').click(function() {
+			$(this).each(function(){
+					$('#object').removeClass();
+					$('#object').addClass("hatch");
+					$('#class-info span').html('.hatch');
+				});
+		});
+
+		$('#bounceBtn').click(function() {
+			$(this).each(function(){
+					$('#object').removeClass();
+					$('#object').addClass("bounce");
+					$('#class-info span').html('.bounce');
+				});
+		});
+
+		$('#pulseBtn').click(function() {
+			$(this).each(function(){
+					$('#object').removeClass();
+					$('#object').addClass("pulse");
+					$('#class-info span').html('.pulse');
+				});
+		});
+
+		$('#floatBtn').click(function() {
+			$(this).each(function(){
+					$('#object').removeClass();
+					$('#object').addClass("floating");
+					$('#class-info span').html('.floating');
+				});
+		});
+
+		$('#tossingBtn').click(function() {
+			$(this).each(function(){
+					$('#object').removeClass();
+					$('#object').addClass("tossing");
+					$('#class-info span').html('.tossing');
+				});
+		});
+
+		$('#pullUpBtn').click(function() {
+			$(this).each(function(){
+					$('#object').removeClass();
+					$('#object').addClass("pullUp");
+					$('#class-info span').html('.pullUp');
+				});
+		});
+
+		$('#pullDownBtn').click(function() {
+			$(this).each(function(){
+					$('#object').removeClass();
+					$('#object').addClass("pullDown");
+					$('#class-info span').html('.pullDown');
+				});
+		});
+
+		$('#stretchLeftBtn').click(function() {
+			$(this).each(function(){
+					$('#object').removeClass();
+					$('#object').addClass("stretchLeft");
+					$('#class-info span').html('.stretchLeft');
+				});
+		});
+
+		$('#stretchRightBtn').click(function() {
+			$(this).each(function(){
+					$('#object').removeClass();
+					$('#object').addClass("stretchRight");
+					$('#class-info span').html('.stretchRight');
+				});
+		});
+});
